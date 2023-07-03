3ff876e7ac2ef980aa8bcd90b33864d2407b57eb
rajkishorepatra <rpatrasm@gmail.com>
2023-07-03 18:58:38 +0530

    NODO Complete

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -3,14 +3,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCrKl1OSs9nu2xEcZ184HZJQt_zkPVbT6Y",
-    authDomain: "basic-todo-app-ee711.firebaseapp.com",
-    databaseURL: "https://basic-todo-app-ee711-default-rtdb.firebaseio.com",
-    projectId: "basic-todo-app-ee711",
-    storageBucket: "basic-todo-app-ee711.appspot.com",
-    messagingSenderId: "1009903618322",
-    appId: "1:1009903618322:web:3ad00f1a7a6987f5d3cbcb",
-    measurementId: "G-Q0FRHQ82X7"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 
